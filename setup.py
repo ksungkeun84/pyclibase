ec130fd0010408138ca7859ff39bb937aa7cac04
Sungkeun Kim <ksungkeun84@tamu.edu>
2021-11-06 16:47:21 -0400

    update setup.cfg and setup.py

---
+++ setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="example-pkg-YOUR-USERNAME-HERE",
+    version="0.1.0",
+    author="Sungkeun Kim",
+    author_email="danguria@gmail.com",
+    description = "A base class of python cli",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/ksungkeun84/pyclibase",
+    project_urls={
+        "Bug Tracker": "https://github.com/ksungkeun84/pyclibase/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.6",
+)
